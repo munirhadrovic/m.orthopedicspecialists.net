6071a2b226d860a584c6c8bcc5c6fdbfe37b96fc
anes89cok <anes89cok@gmail.com>
2015-03-03 16:31:35 +0100

    First commit

---
+++ first/settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="675 West North Avenue, Suite 314"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Melrose Park";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica="IL 60160"; // AKo ne znate skracenicu ostavite praznu
+	$telephone=7084505744;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
